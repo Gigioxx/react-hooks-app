e647af8a35b25bd770927f58a179798049c3417b
Gigiox <guillermo.casanova.b@gmail.com>
2022-04-20 20:54:54 -0400

    Learning about useState hook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
+// import { HookApp } from './HookApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <HookApp /> );
+root.render( <CounterApp /> );
